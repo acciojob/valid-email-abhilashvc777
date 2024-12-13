de0a1bacd5ad15c95cd259c038c721371f640c87
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-13 18:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	let regex = /^[a-zA-Z0-9]+\@[a-z]+\.{1}[a-z]{1,4}\.?[a-z]{0,4}$/
+	if(str.match(regex)){
+		return true
+	}else{
+		return false
+	}
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address.");     
 alert(validEmail(str));
